49367f64aec35b3cb980fc0dc49ca0903da5f8d2
Alex <alexguz8279@gmail.com>
2024-05-05 21:29:11 +0500

    Added type check for keys to make hash table simplier

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <memory>
-#include "../lists/main.cpp"
+// #include "../lists/main.cpp"
+
+class TypeChecker {
+public:
+    static void Check(std::string_view type_name) {
+        if (type_name != typeid(const char*).name() && 
+            type_name != typeid(int).name() &&
+            type_name != typeid(std::string).name() &&
+            type_name != typeid(std::string_view).name()) {
+                
+            std::cout << "Invalid key type - only int or strings\n";
+
+            throw std::bad_typeid();
+        }
+    }
+};
 
 template <class Key, class Value>
 class HashTable;
@@ -10,15 +25,12 @@ class HashNode {
 public:
     HashNode() = delete;
     
-    explicit HashNode(Key key, Value value) : key_{ key }, value_{ value } {}
+    explicit HashNode(Key key, Value value) : key_{ key }, value_{ value } {
+        TypeChecker::Check(typeid(Key).name());
+    }
 
-    friend HashTable;
+    friend HashTable<Key, Value>;
 
-    template<>
-    class HashNode<int, Value> {};
-    
-    template<>
-    class HashNode<std::string_view, Value> {};
 private:
     Key key_;
     Value value_;
@@ -29,11 +41,13 @@ class HashTable {
 public:
 
 private:
-    std::unique_ptr<Value>[]> values; 
+    std::unique_ptr<Value[]> values; 
 
-    size_t GetIndex_()
+    size_t GetIndex_() {
+        return 5;
+    }
 };
 
 int main() {
-    HashNode<bool, int> hash { true, 5 };
+    HashNode<bool, int> hash = HashNode<bool, int>(true, 2);
 }
